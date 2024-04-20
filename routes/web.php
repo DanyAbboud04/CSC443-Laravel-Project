a1fda6c47bfa379c18a005fa4ab22f8c9c101692
DanyAbboud04 <Danymabboud@gmail.com>
2024-04-21 01:55:37 +0300

    added routes for signin, signup, home and login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthManager;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('signin');})->name('signin');
+
+Route::post('/signup', [AuthManager::class, 'signupPost'])->name('signup.post');
+Route::post('/signin', [AuthManager::class, 'signinPost'])->name('signin.post');
+
+Route::get('/home', function () {return view('home');})->name('home');
+Route::get('/logout', function () {return view('logout');})->name('logout');
