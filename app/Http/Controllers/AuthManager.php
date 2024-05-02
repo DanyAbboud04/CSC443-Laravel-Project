44c433ec053ecbc61c78a1be7c6e969cabbe90c5
DanyAbboud04 <Danymabboud@gmail.com>
2024-05-02 13:19:34 +0300

    added redirections for user, and admin login and signup

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -17,11 +17,11 @@ class AuthManager extends Controller
             'password' => 'required',
         ]);
         $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)){
-            return redirect()->intended(route('home'));
-        }
-        else if($credentials['email']==='admin' && $credentials['password']==='admin'){
+        if($credentials['email']==='admin@gmail.com' && $credentials['password']==='admin'){
             return redirect()->intended(route('admin'));
+        }
+        else if (Auth::attempt($credentials)){
+            return redirect()->intended(route('home'));
         }
         else{
             return redirect()->route('signin')->with('error', 'Invalid Email or Password');
@@ -35,24 +35,23 @@ class AuthManager extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required',
         ]);
-
+    
         $data['first_name'] = $request->fname;
         $data['last_name'] = $request->lname;
         $data['email'] = $request->email;
         $data['password'] = Hash::make($request->password); 
-
+        $data['is_admin'] = $request->email === 'admin@gmail.com' && $request->password === 'admin';
+    
         $user = User::create($data);
-
+    
         if(!$user){
-            return redirect()->route('signin')->with('error', 'Error');
+            return redirect()->route('signin')->with('error', 'Registration failed');
         }
-        return redirect()->route('signin')->with('success', 'Done, sign in please');
+        return redirect()->route('signin')->with('success', 'Registration successful. Please sign in.');
     }
-
     function logout(){
         Session::flush();
         Auth::logout();
         return redirect(route('/'));
-
     }
 }
