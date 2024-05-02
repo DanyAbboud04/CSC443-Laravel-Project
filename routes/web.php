4b0f0cf932b1c73e6a2a0578d59cbd99daac11e2
DanyAbboud04 <Danymabboud@gmail.com>
2024-05-02 13:21:02 +0300

    added route for admin page, it does the useController index function and added enter as guest route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthManager;
+use App\Http\Controllers\UserController; 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('signin');})->name('signin');
@@ -11,3 +13,24 @@ Route::post('/signin', [AuthManager::class, 'signinPost'])->name('signin.post');
 Route::get('/home', function () {return view('home');})->name('home');
 Route::get('/logout', function () {return view('logout');})->name('logout');
 Route::get('/admin', function () {return view('admin');})->name('admin');
+
+
+Route::get('/admin', [UserController::class, 'index'])->name('admin');
+
+
+Route::get('/enter-as-guest', function (Request $request) {
+    $guestUser = User::create([
+        'first_name' => 'Guest',
+        'last_name' => 'User',
+        'email' => 'guest' . rand() . '@example.com',  
+        'password' => '', 
+        'is_admin' => false,
+        'is_active' => false,
+        'is_guest' => true,  
+    ]);
+
+    // Optionally, log in the user as a guest
+    Auth::login($guestUser);
+
+    return redirect()->route('home');  // Redirect to the desired location
+});
