de3e8964f2ef03cffd3b6ab8b65cd8c60c592e57
DanyAbboud04 <Danymabboud@gmail.com>
2024-05-03 16:26:00 +0300

    added function that gets single user, and method to update email or name or reset password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,18 +1,61 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\User;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     // UserController.php
 
-public function index()
+    public function index()
+    {
+        $users = User::all(); // get all users from db
+        return view('admin', compact('users')); // pass all users to the view admin
+    }
+
+    public function getUser()
+    {
+        $user = Auth::user();  //get current user
+        return view('profile', compact('user'));
+    }
+
+    public function updateFirstName(Request $request)
 {
-    $users = User::all(); // Fetch all users from the database
-    return view('admin', compact('users')); // Pass users to the view
+    $user = Auth::user();
+    $user->first_name = $request->first_name;
+    $user->save();
+
+    return back()->with('success', 'First name updated successfully!');
 }
 
+public function updateLastName(Request $request)
+{
+    $user = Auth::user();
+    $user->lastt_name = $request->lastt_name;
+    $user->save();
+
+    return back()->with('success', 'Last name updated successfully!');
+}
+
+public function updateEmail(Request $request)
+{
+    $user = Auth::user();
+    $user->email = $request->email;
+    $user->save();
+
+    return back()->with('success', 'Email updated successfully!');
+}
+
+
+public function resetPassword(Request $request)
+{
+    $user = Auth::user();
+    $user->password = Hash::make($request->new_password);  
+    $user->save();
+
+    return back()->with('success', 'Password reset successfully!');
+}
 }
