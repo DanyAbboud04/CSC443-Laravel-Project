2e0db0258ad5b349fcf9efcedaaa972d8289907d
DanyAbboud04 <Danymabboud@gmail.com>
2024-05-03 16:25:15 +0300

    added route to profile, route that gets user and routes to edit name, emial and reset password

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/logout', function () {return view('logout');})->name('logout');
 Route::get('/admin', function () {return view('admin');})->name('admin');
 
 
-Route::get('/admin', [UserController::class, 'index'])->name('admin');
+Route::get('/admin', [UserController::class, 'index'])->name('admin');   //route takes get method, index  method bl usercontroller class
 
 
 Route::get('/enter-as-guest', function (Request $request) {
@@ -37,3 +37,12 @@ Route::get('/enter-as-guest', function (Request $request) {
 
 Route::post('/toggle-user-status/{id}', [AuthManager::class, 'toggleUserStatus'])->name('toggle-user-status'); //route takes post method, takes id tabaa l user w bt rouh aa toggleUserStatus  method bl authmanager class
 Route::delete('/delete-guest/{id}', [AuthManager::class, 'deleteGuest'])->name('delete-guest');  //nafs l chi hon bas la delete
+
+Route::get('/profile', function () {return view('profile');})->name('profile');
+
+Route::get('/profile', [UserController::class, 'getUser'])->name('profile');
+
+Route::post('/profile/update/firstname', [UserController::class, 'updateFirstName'])->name('update.firstname');  //route takes post method tabaa update/firstname eno mtl update.firstname, bt rouh aal ontroller tabaa user w bt aayet lal method updateFirstName
+Route::post('/profile/update/lastname', [UserController::class, 'updateLasttName'])->name('update.lastname');
+Route::post('/profile/update/email', [UserController::class, 'updateEmail'])->name('update.email');
+Route::post('/profile/update/password', [UserController::class, 'resetPassword'])->name('update.password');
